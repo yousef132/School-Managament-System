c8b7702f75f5b618c2d32d9b8c4f7c363f3be88b
Yousef Saad <saady8454@gmail.com>
2024-06-24 01:04:03 +0300

    Implement AutoMapper with partial classes for separation of concerns

---
+++ SchoolManagment.Core/Mapping/Students/StudentProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SchoolManagment.Core.Features.Students.Queries.Responses;
+using SchoolManagment.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+namespace SchoolManagment.Core.Mapping.Students
+{
+	public partial class StudentProfile : Profile
+	{
+		public StudentProfile()
+		{
+			GetStudentsMapping();
+		}
+	}
+}
